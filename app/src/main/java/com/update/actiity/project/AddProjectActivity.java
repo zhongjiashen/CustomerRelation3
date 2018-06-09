8ac113f60817f7015b154ca18c45f1e6c5a591b3
shenzhongjia <shen123456>
2018-06-09 16:10:26 +0800

    shen

--- app/src/main/java/com/update/actiity/project/AddProjectActivity.java
+++ app/src/main/java/com/update/actiity/project/AddProjectActivity.java
@@ -289,10 +289,10 @@ public class AddProjectActivity extends BaseActivity {
             showShortToast("请先选择部门");
             return;
         }
-        if (TextUtils.isEmpty(mEmpid)) {
-            showShortToast("请先选择业务员");
-            return;
-        }
+//        if (TextUtils.isEmpty(mEmpid)) {
+//            showShortToast("请先选择业务员");
+//            return;
+//        }
         mMap.put("billid", "0");//主键ID;0或空表示新增
         mMap.put("billdate", tvDocumentDate.getText().toString()
         );//单据日期
