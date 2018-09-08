e9ebdd544db92ecb1227031d436fe00634d9264f
shenzhongjia <shen123456>
2018-09-08 10:37:39 +0800

    shen

--- app/src/main/java/com/cr/activity/jxc/xsgl/xskd/JxcXsglXskdAddActivity.java
+++ app/src/main/java/com/cr/activity/jxc/xsgl/xskd/JxcXsglXskdAddActivity.java
@@ -582,9 +582,28 @@ public class JxcXsglXskdAddActivity extends BaseActivity {
                     list.addAll((List<Map<String, Object>>) data.getExtras().getSerializable("list"));
                     yyList.clear();
                     yyList.addAll((List<Map<String, Object>>) data.getExtras().getSerializable("list"));
-                    xzspnumTextview.setText("共选择了" + list.size() + "商品");
-                    hjjeEdittext.setText(data.getExtras().getString("totalAmount"));
-                    adapter.notifyDataSetChanged();
+                    if (list != null) {
+                        for (int i = 0; i < list.size(); i++) {
+                            UUID uuid = UUID.randomUUID();
+                            list.get(i).put("serialinfo", uuid.toString().toUpperCase());
+                            list.get(i).put("serials", new ArrayList<Serial>());//
+                            list.get(i).put("taxrate", mTaxrate);
+                            Double csje = Double.parseDouble(list.get(i).get("unitprice").toString()) * (Double.parseDouble(mTaxrate) + 100) / 100;
+                            list.get(i).put("taxunitprice", FigureTools.sswrFigure(csje));
+                            String amount = (csje
+                                    * Double.parseDouble(list.get(i).get("unitqty").toString())) + "";
+                            list.get(i).put("amount", FigureTools.sswrFigure(amount + ""));
+                        }
+                        double je = 0;
+                        for (Map<String, Object> m : list) {
+                            je += Double.parseDouble(m.get("amount").toString());
+                        }
+                        xzspnumTextview.setText("共选择了" + list.size() + "商品");
+                        hjjeEdittext.setText("￥" + FigureTools.sswrFigure(je + "") + "");
+                        adapter.notifyDataSetChanged();
+                    }
+
+
                     break;
                 case 6:
                     if (!ckckEdittext.getText().toString().equals(data.getExtras().getString("dictmc"))) {
