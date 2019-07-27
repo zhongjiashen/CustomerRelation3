c1be1b6c8b41775bf823efafdf9a431c117f0604
shenzhongjia <shen123456>
2019-07-27 16:14:39 +0800

    添加类库filter-lib

---
+++ filter-lib/src/main/java/com/baiiu/filter/interfaces/OnFilterDoneListener.java
@@ -0,0 +1,12 @@
+package com.baiiu.filter.interfaces;
+
+import java.util.Map;
+
+/**
+ * author: baiiu
+ * date: on 16/1/21 23:30
+ * description:
+ */
+public interface OnFilterDoneListener {
+    void onFilterDone(int position, Map map);
+}
